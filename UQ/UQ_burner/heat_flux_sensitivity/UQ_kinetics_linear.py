20249bfeab425b03158280125102f353c32e0be5
Tulio <tulioricci@gmail.com>
2025-07-20 08:43:14 -0500

    Double check script for UQ

--- UQ/UQ_burner/heat_flux_sensitivity/UQ_kinetics_linear.py
+++ UQ/UQ_burner/heat_flux_sensitivity/UQ_kinetics_linear.py
@@ -4,32 +4,36 @@ import cantera as ct
 import sys
 
 #mech = "wang99_51sp"
-mech = "uiuc_20sp_Troe"
+mech = "uiuc_20sp"
 
 ####################################
 
-if mech == "wang99_51sp":
-    new_A_parameters = [(2.4770000000000003e+27, 12700000000000.002), 83000000000.00002, 47600.00000000001, (2.565e+24, 386000000.0), 15000000000.000002, 3600.0000000000005, 187000000000000.03, (1.2000000000000001e+36, 1080000000.0000002), 50700.00000000001, 4990000000.000001]
-    new_b_parameters = [(-4.76, -0.63),  0.0,  1.228,  (-3.4, 1.62),  0.0,  2.0,  -1.0,  (-7.62, 0.454),  1.93,  0.1]
-    new_E_parameters = [(10208960.0, 1602472.0000000002),  60303992.00000001,  292880.0, (149781844.48000002, 155009752.48000002), 2510400.0, 10460000.000000002, 71128000.0, (29162480.0, 7614880.000000001), 54182800.0, 44350400.0]
-    list_of_reactions = [84, 1, 30, 151, 63, 224, 49, 219, 220, 102]
+#if mech == "wang99_51sp":
+#    new_A_parameters = [(2.4770000000000003e+27, 12700000000000.002), 83000000000.00002, 47600.00000000001, (2.565e+24, 386000000.0), 15000000000.000002, 3600.0000000000005, 187000000000000.03, (1.2000000000000001e+36, 1080000000.0000002), 50700.00000000001, 4990000000.000001]
+#    #new_b_parameters = [(-4.76, -0.63),  0.0,  1.228,  (-3.4, 1.62),  0.0,  2.0,  -1.0,  (-7.62, 0.454),  1.93,  0.1]
+#    #new_E_parameters = [(10208960.0, 1602472.0000000002),  60303992.00000001,  292880.0, (149781844.48000002, 155009752.48000002), 2510400.0, 10460000.000000002, 71128000.0, (29162480.0, 7614880.000000001), 54182800.0, 44350400.0]
+#    #list_of_reactions = [84, 1, 30, 151, 63, 224, 49, 219, 220, 102]
 
 if mech == "uiuc_20sp":
-    new_A_parameters = [(2.4770000000000003e+27, 12700000000000.002), 26440000000000.004, 187000000000000.03, 47600.00000000001, 591.6000000000001, 84300000000.00002, 4.400000000000001e+16, 7.8e+41, 16520000000000.004, 46000000000000.01]
-    new_b_parameters = [(-4.76000000e+00, -6.30000000e-01), -6.70700000e-01, -1.00000000e+00, 1.22800000e+00, 2.43300000e+00, 0.00000000e+00, -2.00000000e+00, -9.14700000e+00, -7.60000000e-01, -1.39000000e+00]
-    new_E_parameters = [(1.02089600e+07, 1.60247200e+06), 7.12995440e+07, 7.11280000e+07, 2.92880000e+05, 2.23852368e+08, 0.00000000e+00, 0.00000000e+00, 1.96229600e+08, 0.00000000e+00, 4.22584000e+06]
-    list_of_reactions = [69, 1, 25, 18, 12, 37, 5, 50, 10, 47]
+    new_A_parameters = [2.644000e+13, 4.760000e+04, ( 2.477000e+27, 1.270000e+13 ), 1.870000e+14, 8.430000e+10, 7.600000e+09, 1.652000e+13, 7.485000e+10, 4.600000e+13, 3.000000e+10]
+    #new_b_parameters = [(-4.76000000e+00, -6.30000000e-01), -6.70700000e-01, -1.00000000e+00, 1.22800000e+00, 2.43300000e+00, 0.00000000e+00, -2.00000000e+00, -9.14700000e+00, -7.60000000e-01, -1.39000000e+00]
+    #new_E_parameters = [(1.02089600e+07, 1.60247200e+06), 7.12995440e+07, 7.11280000e+07, 2.92880000e+05, 2.23852368e+08, 0.00000000e+00, 0.00000000e+00, 1.96229600e+08, 0.00000000e+00, 4.22584000e+06]
+    #list_of_reactions = [69, 1, 25, 18, 12, 37, 5, 50, 10, 47]
+    list_of_reactions = [1, 18, 69, 25, 37, 26, 10, 14, 47, 44]
+    
+
+
 
 nreactions = len(list_of_reactions)
 if len(new_A_parameters) != nreactions:
     print("new_A_parameters is wrong")
     sys.exit()
-if len(new_b_parameters) != nreactions:
-    print("new_b_parameters is wrong")
-    sys.exit()
-if len(new_E_parameters) != nreactions:
-    print("new_E_parameters is wrong")
-    sys.exit()
+#if len(new_b_parameters) != nreactions:
+#    print("new_b_parameters is wrong")
+#    sys.exit()
+#if len(new_E_parameters) != nreactions:
+#    print("new_E_parameters is wrong")
+#    sys.exit()
 
 volume_per_min = 25
 width = 0.010
@@ -84,7 +88,7 @@ for ii, _ireact in enumerate(list_of_reactions):
     
     #if rxn_type == "Arrhenius" or rxn_type == "three-body-Arrhenius":
     if type(new_A_parameters[ii]) is float:
-        new_A = custom_reactions[ireact].input_data['rate-constant']['A']
+        new_A = new_A_parameters[ii]
         new_b = custom_reactions[ireact].input_data['rate-constant']['b']
         new_E = custom_reactions[ireact].input_data['rate-constant']['Ea']
         custom_reactions[ireact] = ct.Reaction(
@@ -95,12 +99,12 @@ for ii, _ireact in enumerate(list_of_reactions):
     
     #if rxn_type == "falloff-Troe":
     if type(new_A_parameters[ii]) is tuple:
-        new_A_low = custom_reactions[ireact].input_data['low-P-rate-constant']['A']
+        new_A_low = new_A_parameters[ii][0]
         new_b_low = custom_reactions[ireact].input_data['low-P-rate-constant']['b']
         new_E_low = custom_reactions[ireact].input_data['low-P-rate-constant']['Ea']
         low = ct.Arrhenius(new_A_low, new_b_low, new_E_low)
 
-        new_A_high = custom_reactions[ireact].input_data['high-P-rate-constant']['A']
+        new_A_high = new_A_parameters[ii][1]
         new_b_high = custom_reactions[ireact].input_data['high-P-rate-constant']['b']
         new_E_high = custom_reactions[ireact].input_data['high-P-rate-constant']['Ea']        
         high = ct.Arrhenius(new_A_high, new_b_high, new_E_high)
@@ -198,24 +202,22 @@ for phi in phi_array:
         try:
             sim.inlet.mdot = rhoU_ref
             
-            sim.set_refine_criteria(ratio=4, slope=0.25, curve=0.25,
+            sim.set_refine_criteria(ratio=4, slope=0.15, curve=0.15,
                                     prune=0.1)
             sim.solve(loglevel, refine_grid=True, auto=True)
 
-            sim.set_refine_criteria(ratio=3, slope=0.15, curve=0.15,
+            sim.set_refine_criteria(ratio=3, slope=0.10, curve=0.10,
                                     prune=0.05)
-            sim.solve(loglevel, refine_grid=True, auto=True)
-
-            sim.set_refine_criteria(ratio=ratio, slope=0.10, curve=0.10,
-                                    prune=0.025)
             sim.solve(loglevel, refine_grid=True, auto=True)
 
             sim.set_refine_criteria(ratio=ratio, slope=0.05, curve=0.05,
                                     prune=0.025)
             sim.solve(loglevel, refine_grid=True, auto=True)
 
-            sim.set_refine_criteria(ratio=ratio, slope=0.03, curve=0.03)
-            sim.solve(1, refine_grid=True, auto=True)
+            sim.set_refine_criteria(ratio=ratio, slope=slope, curve=curve,
+                                    prune=0.025)
+            sim.solve(loglevel, refine_grid=True, auto=True)
+
 
             dT = sim.T[-2] - sim.T[-1]
             dx = sim.grid[-2] - sim.grid[-1]
@@ -243,4 +245,4 @@ for phi in phi_array:
         data = [flame_speed, temperature, X_CO, X_CO2, flux]
         np.savetxt(result_file, data, fmt="%s")
         
-        sim.save("debug" + str('%4.2f' % phi) + ".csv", basis="mole", overwrite=True)
+        #sim.save("debug" + str('%4.2f' % phi) + ".csv", basis="mole", overwrite=True)
